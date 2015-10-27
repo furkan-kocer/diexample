63d247bb7f4ae33b00c0574ebde4a830c3dcd41d
John Thompson <john@springframework.guru>
2015-10-27 19:25:39 -0400

    added java-config example

--- src/main/java/guru/springframework/services/HelloWorldServiceEnglishImpl.java
+++ src/main/java/guru/springframework/services/HelloWorldServiceEnglishImpl.java
@@ -1,14 +1,9 @@
 package guru.springframework.services;
 
-import com.sun.org.glassfish.external.probe.provider.annotations.ProbeProvider;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by jt on 10/26/15.
  */
-@Component
-@Profile("english")
 public class HelloWorldServiceEnglishImpl implements HelloWorldService{
     @Override
     public String getGreeting() {
--- src/main/java/guru/springframework/services/HelloWorldServiceSpanishImpl.java
+++ src/main/java/guru/springframework/services/HelloWorldServiceSpanishImpl.java
@@ -1,14 +1,9 @@
 package guru.springframework.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
 
 /**
  * Created by jt on 10/26/15.
  */
-@Component
-@Profile("spanish")
 public class HelloWorldServiceSpanishImpl implements HelloWorldService {
 
     @Override
