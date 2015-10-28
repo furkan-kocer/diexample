d8fcfc1f2ea6fe997486f21babd097208c9e2cac
John Thompson <john@springframework.guru>
2015-10-28 14:07:10 -0400

    added factory bean example

--- src/main/java/guru/springframework/controllers/GreetingController.java
+++ src/main/java/guru/springframework/controllers/GreetingController.java
@@ -2,6 +2,7 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -12,9 +13,25 @@ public class GreetingController {
 
     private HelloWorldService helloWorldService;
 
+    private HelloWorldService helloWorldServiceGerman;
+
+    private HelloWorldService helloWorldServiceFrench;
+
     @Autowired
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
+    }
+
+    @Autowired
+    @Qualifier("helloWorldServiceGerman")
+    public void setHelloWorldServiceGerman(HelloWorldService helloWorldServiceGerman) {
+        this.helloWorldServiceGerman = helloWorldServiceGerman;
+    }
+
+    @Autowired
+    @Qualifier("french")
+    public void setHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench) {
+        this.helloWorldServiceFrench = helloWorldServiceFrench;
     }
 
     public String sayHello(){
@@ -22,6 +39,8 @@ public class GreetingController {
         String greeting = helloWorldService.getGreeting();
 
         System.out.println(greeting);
+        System.out.println(helloWorldServiceGerman.getGreeting());
+        System.out.println(helloWorldServiceFrench.getGreeting());
 
         return greeting;
     }
